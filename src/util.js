6bebc633261edcfef04310d2dbf735dcc0aa9fd5
Khalid Williams <khalidwill27@gmail.com>
2019-11-14 19:48:37 -0700

    Move cleaning functions to util file, export all functions

--- src/util.js
+++ src/util.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports = function getData(baseURL, path, query) {
+function getData(baseURL, path, query) {
   let fullUrl = "";
   query? fullUrl = `${baseURL}${path}?apiKey=${process.env.KEY}&${query}` : fullUrl = `${baseURL}${path}?apiKey=${process.env.KEY}`
   const promise = fetch(fullUrl, {
@@ -13,4 +13,51 @@ module.exports = function getData(baseURL, path, query) {
     },
   });
   return promise;
+}
+
+function cleanIngredient(ingredient) {
+  return {
+    id: ingredient.id,
+    name: ingredient.name,
+    estimatedCost: ingredient.estimatedCost
+  }
+}
+
+function cleanRecipes(dirtyRecipes) {
+ // This may no longer work the way I had originally intended. May need to refactor if pulling all interaction into index.js 
+	
+  let recipes = dirtyRecipes.recipes;
+  let cleanedRecipes = recipes.map(recipe => {
+    let cleanRecipe = {
+      name: recipe.title,
+      ingredients: [],
+      instructions: [],
+      tags: recipe.dishTypes
+    };
+    
+    cleanRecipe.ingredients = recipe.extendedIngredients.map( ingredient => {
+      return {
+        name: ingredient.name,
+        id: ingredient.id,
+        quanitity: {
+	  amount: ingredient.amount,
+	  unit: ingredient.unit,
+	},
+      }
+    });
+//    console.log(recipe.analyzedInstructions);
+    cleanRecipe.instructions = recipe.analyzedInstructions[0].steps.map(step => {
+      return {
+        number: step.number,
+	instruction: step.step
+      }})
+   return cleanRecipe;
+  })
+  return cleanedRecipes;
+}
+
+module.exports = {
+  getData,
+  cleanIngredient,
+  cleanRecipes
 }
