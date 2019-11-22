1e51fb800048d4155f10cc7de041d61e0272c88f
Khalid Williams <khalidwill27@gmail.com>
2019-11-22 16:20:32 -0700

    Make users

--- src/users.js
+++ src/users.js
@@ -2,14 +2,13 @@ import fs from 'fs';
 import {dirname} from 'path';
 import { fileURLToPath } from 'url';
 import faker from 'faker';
-import allIngredients from './output/ingredient-data.js'; 
-import allRecipes from './output/recipe-data.js';
-import { getIdsForAllIngredients } from './src/utils.js';
+// import allingredientinfo from './output/ingredient-data.js'; 
+import allRecipes from '../output/recipe-data.js';
+import { getIdsForAllIngredients } from './util.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
-let users = [];
 let ingredientIds = getIdsForAllIngredients(allRecipes);
 
 function getRandomNumberOfRandomIds(ids) {
@@ -19,20 +18,35 @@ function getRandomNumberOfRandomIds(ids) {
   for (let i = 0; i < numberOfIdsToTake; i++){
     // get a random id, and give it a random number from 1 - 5 for the amount
     let idOfElementToGrab = Math.floor(Math.random() * numberOfIds);
-    let amountOfIngredient = Math.floor(Math.random() * 5)
+    let amountOfIngredient = Math.floor(Math.random() * 5) + 1;
     let elementToGrab = ids[idOfElementToGrab];
     allItems.push({ingredient: elementToGrab, amount: amountOfIngredient});
   }
   return allItems;
 }
 
-for (let i = 1; i < 50; i++){
-  let user = {};
-  user.id = i;
-  user.name = faker.name.firstName + faker.name.lastName;
-  user.pantry = getRandomNumberOfRandomIds(ingredientIds); 
-  user.push(user);
+function makeUsers () {
+  let users = [];
+  for (let i = 1; i < 50; i++){
+    let user = {};
+    user.id = i;
+    user.name = faker.name.firstName() + ' ' + faker.name.lastName();
+    user.pantry = getRandomNumberOfRandomIds(ingredientIds); 
+    users.push(user);
+  }
+  return users;
 }
 
-// Write the file next
-// fs.writeFile
+function outputUsersFile (usersData) {
+  let stringifiedUsers = JSON.stringify(usersData);
+  fs.writeFile(__dirname + '/../output/users-data.js', stringifiedUsers, (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('users file written'); 
+    }
+  }) 
+}
+
+let users = makeUsers();
+outputUsersFile(users);
