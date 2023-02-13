fe4447ec9c8f098e130b5a643d55063e10ec3b61
Robbie Jaeger <jaeger.rob@gmail.com>
2023-02-13 15:15:31 -0700

    Remove pantry and add user recipes

--- src/users.js
+++ src/users.js
@@ -2,28 +2,8 @@ import fs from 'fs';
 import {dirname} from 'path';
 import { fileURLToPath } from 'url';
 import faker from 'faker';
-// import allingredientinfo from './output/ingredient-data.js'; 
-import allRecipes from '../output/recipe-data.js';
-import { getIdsForAllIngredients } from './util.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
-
-let ingredientIds = getIdsForAllIngredients(allRecipes);
-
-function getRandomNumberOfRandomIds(ids) {
-  let allItems = [];
-  let numberOfIds = ids.length;
-  let numberOfIdsToTake = Math.floor(Math.random() * numberOfIds);
-  for (let i = 0; i < numberOfIdsToTake; i++){
-    // get a random id, and give it a random number from 1 - 5 for the amount
-    let idOfElementToGrab = Math.floor(Math.random() * numberOfIds);
-    let amountOfIngredient = Math.floor(Math.random() * 5) + 1;
-    let elementToGrab = ids[idOfElementToGrab];
-    allItems.push({ingredient: elementToGrab, amount: amountOfIngredient});
-  }
-  return allItems;
-}
 
 function makeUsers () {
   let users = [];
@@ -31,14 +11,14 @@ function makeUsers () {
     let user = {};
     user.id = i;
     user.name = faker.name.firstName() + ' ' + faker.name.lastName();
-    user.pantry = getRandomNumberOfRandomIds(ingredientIds); 
+    user.recipesToCook = []; 
     users.push(user);
   }
   return users;
 }
 
 function outputUsersFile (usersData) {
-  let stringifiedUsers = JSON.stringify(usersData);
+  let stringifiedUsers = JSON.stringify(usersData, null, 2);
   fs.writeFile(__dirname + '/../output/users-data.js', stringifiedUsers, (err) => {
     if (err) {
       console.log(err)
